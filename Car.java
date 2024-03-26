bbd4a5d9a5007fe42cdfbb00685f4801e2fdb24d
dlew137 <163804956+dlew137@users.noreply.github.com>
2024-03-25 22:00:51 -0500

    Update Car.java

--- Car.java
+++ Car.java
@@ -1,16 +1,49 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
+package prog01_aorderedlist;
+import java.util.Scanner;
 
 /**
  *
  * @author diamond
  */
-class Car {
+public class Car {
+    private String make;
+    private int year;
+    private int price;
 
-    Car(String make, int year, int price) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+
+    public Car(String make, int year, int price) {
+        this.make = make;
+        this.year = year;
+        this.price = price;
     }
-    
+
+    public String getMake() {
+        return make;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int compareTo(Car other) {
+        if (!this.make.equals(other.make)) {
+            return this.make.compareTo(other.make);
+        } else {
+            if (this.year != other.year) {
+                return this.year - other.year;
+            } else {
+                return 0;
+            }
+        }
+
+    }
+
+    public String toString() {
+        return "Make: " + make + ", Year:" + year + ", Price: " + price + ";";
+    }
 }
